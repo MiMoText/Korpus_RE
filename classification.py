7ac1e640bb97a722cfb133afaf9361003b107a6a
Katharina Dietz <dietz@uni-trier.de>
2021-10-15 16:08:44 +0200

    added classif added classification.py

---
+++ classification.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import create_korpus_re
+
+# gibt es Sinn als einfachste Variante erstmal eine
+# binäre Klassifikation zu implementieren, also relation - keine relation?
+# ich glaube nicht
+
+my_dataframe = create_korpus_re.my_column
+
