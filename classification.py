0c3f59470a33c9fd25b58bf140fb950fd6608737
Katharina Dietz <dietz@uni-trier.de>
2021-10-20 19:10:12 +0200

    added classif worked on classification.py

--- classification.py
+++ classification.py
@@ -1,9 +1,28 @@
 import pandas as pd
 import create_korpus_re
+from collections import Counter
 
 # gibt es Sinn als einfachste Variante erstmal eine
 # binäre Klassifikation zu implementieren, also relation - keine relation?
 # ich glaube nicht
 
-my_dataframe = create_korpus_re.my_column
+df = pd.read_pickle("my_buffer/korpus_entities.infer")
+#print(df)
+#print(df.columns)
+# most frequent words in relation
+
+# print(df.loc["relation out of scope"])
+df_test = df.set_index("Relation")
+print(df_test)
+
+
+df_topic_interest = df[df["Code des Typs"] == "topicInterest"]
+
+list_topic_interest = df_topic_interest["Relation"].tolist()
+print(list_topic_interest)
+
+print(Counter(list_topic_interest))
+# man sieht, dass sich hier noch gar nichts doppelt
+# --> bag of word für weitere Einblicke
+
 
