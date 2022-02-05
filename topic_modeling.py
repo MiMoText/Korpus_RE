f840e251bc76e917ae43b62f1952e2b3cebe7446
Luckyz7 <luckyz7@pop-os.localdomain>
2022-02-05 22:12:50 +0100

    Update: Proc files individually, assumed topics/file:3
    Words per topic: 75. Added french stopwords & output files @ data_out/lda
    Docs > 1M chars are split evenly and processed individually (spacy bottle-
    neck). Some possible redundancies. No serios fine-tuning as of now.

--- topic_modeling.py
+++ topic_modeling.py
@@ -1,103 +1,124 @@
-import numpy as np
-import pandas as pd
 import glob
-import time
+import textwrap
+from tqdm import tqdm
+from pprint import pprint
+
 import gensim
 import gensim.corpora as corpora
-from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 
 import spacy
-#from spacy.lemmatizer import Lemmatizer
-from spacy.lang.de.stop_words import STOP_WORDS
+from spacy.lang.fr.stop_words import STOP_WORDS
 from spacy.language import Language
-#import en_core_web_lg
 
-from tqdm import tqdm
-from pprint import pprint
-#------------------------------------------------------------------------------
+#-----------------------Prep Raw Text-----------------------------------------------------
 nlp = spacy.load('de_core_news_lg')
 txt_paths = glob.glob("data_in/lda/*.txt")  # Save the (relative) paths of all .txt files in a list
-print("{} text file(s) have been found. \n".format(len(txt_paths)))
 print(txt_paths)
+print("{} text file(s) have been found. \n".format(len(txt_paths)))
 
 #iterate over all files in data_in/lda and read them into a list
 txt_file_list = []
 for txt_path in txt_paths:
+
+    filename = txt_path.split("/")[-1]
     with open(txt_path, 'r') as f:
-        txt_file_list.append(f.read().replace('\n', ' '))
+        prepared_text = f.read()
+        prepared_text = prepared_text.replace('\n', ' ').replace('\t', ' ').replace("  "," ").replace('ſ', 's') # old german "s"-sign
+        txt_file_list.append((prepared_text,filename)) # add the filename as a second element to the tuple
 
-for txt_file in txt_file_list:
-    print("text file len:",len(txt_file))
+print("Splitting files that are too long into multiple files...")
+
+txt_file_list_aux = txt_file_list # make an aux file so that we can delete from original list if needed
+
+for txt_file in txt_file_list_aux:
+    if len(txt_file[0]) > 1000000: # if the text file is longer than 1 million characters (spacy can't handle that)
+
+        file_len = len(txt_file[0])
+        mcount = 1 # million count. mcount is used to name the new files, and split text evenly
+                        # example : 2.4 million characters -> 3x ~0.8 million characters
 
 
-# costum stopword list
-stop_list = ["de","et","la","le","les","que","à","<",">","\t","all","S.","d´", ] #Examples:"Mrs.","Ms.","say","WASHINGTON","'s","Mr."
+        while file_len/(1000000*mcount) > 1:
+            mcount += 1
 
-# Updates spaCy's default stop words list with my additional words.
+
+        split_len = int(file_len/(mcount)) +10 # add 10 to account for int rounding
+        print("Splitting file {} into {} files of approx. {} characters each. \n".format(txt_file[1], mcount, split_len))
+
+        split_text = textwrap.wrap(txt_file[0], split_len) # text is split into a list of strings
+                                                                # textwrap should split on whitespaces
+
+        filename = txt_file_list.pop(txt_file_list.index(txt_file))[1] # remove the original file from the list (not aux list)
+                                                                            # save the original file name from tupel
+        for i in range(len(split_text)):
+            txt_file_list.append((split_text[i], filename+"_"+str(i+1)))    # add the split text list to the list
+
+print("Text tuples processed \n\n")
+
+#-----------------Prep Pipeline------------------------------------------------------------------
+# Additional stopword list --> Updates spaCy's default stop words list with my additional words.
+stop_list = ['welch','sogar'] # add your own stop words here
 nlp.Defaults.stop_words.update(stop_list)
 
-# Iterates over the words in the stop words list and resets the "is_stop" flag.
+# Iterates over the words in the stop words list (french) and resets the "is_stop" flag.
 for word in STOP_WORDS:
     lexeme = nlp.vocab[word]
     lexeme.is_stop = True
 
+
+# Since spacy 3.0, we need decorators to register components...
 @Language.component("custom_lemma") # This decorator registers the component with spaCy.
-def custom_lemma(doc): # Check if this is the case for german***
+def custom_lemma(doc):
     # This takes in a doc of tokens from the NER and lemmatizes them.
     # Pronouns (like "I" and "you" get lemmatized to '-PRON-', so I'm removing those.
     doc = [token.lemma_ for token in doc if token.lemma_ != '-PRON-']
     doc = u' '.join(doc) # u for unicode?
     return nlp.make_doc(doc)
 
+# The add_pipe function appends our functions to the default pipeline. (would not word this way without decorators)
 nlp.add_pipe("custom_lemma",after='ner')
 
 @Language.component("remove_stopwords") # This decorator registers the component with spaCy.
 def remove_stopwords(doc):
     # This will remove stopwords and punctuation.
-    # Use token.text to return strings, which we'll need for Gensim.
-    doc = [token.text for token in doc if token.is_stop != True and token.is_punct != True]
+    # Use token.text to return !strings!, which we'll need for Gensim.
+    doc = [token.text for token in doc if token.is_stop != True and token.is_punct != True and len(token.text) > 3]
     return doc
 
 nlp.add_pipe("remove_stopwords", last=True)
 
+print("The nlp pipeline has been set up:")
 print(nlp.pipe_names)
 
-# The add_pipe function appends our functions to the default pipeline.
-#nlp.add_pipe(lemmatizer,name='lemmatizer',after='ner') got Error: [E966]
-#`nlp.add_pipe` now takes the string name of the registered component factory, not a callable component. Expected string, but got <function lemmatizer at 0x7fbca03d61f0> (name: 'lemmatizer').
-
-
-
-
-doc_list = []
-# Iterates through each article in the corpus.
+#-----------------LDA------------------------------------------------------------------
+# Iterates through each doc in our list
 for doc in tqdm(txt_file_list):
-    # Passes that article through the pipeline and adds to a new list.
-    if len(doc) > 1000000:
-        pass
-    else:
+    print("\n-------------Topics in file {}---------------\n".format(doc[1]))
+    # Passes that article through the pipeline
+    pr = nlp(doc[0])
 
-        pr = nlp(doc)
-        #print("this pr: ",pr)
-        #stop for 5 sec
-       #time.sleep(5)
-        doc_list.append(pr)
+    # Creates gensim corpus, which is a mapping of word IDs to words.
+            #It is used to determine the vocabulary size, as well as for debugging and topic printing.
+    words = corpora.Dictionary([pr])# note that pr is passed as list
 
+    # Turns each document into a bag of words.
+    corpus = [words.doc2bow(pr)]
 
-# Creates, which is a mapping of word IDs to words.
-words = corpora.Dictionary(doc_list)
-
-# Turns each document into a bag of words.
-corpus = [words.doc2bow(doc) for doc in doc_list]
-
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+    # We'll assume that the number of topics is 3 for now.
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                            id2word=words,
-                                           num_topics=10,
+                                           num_topics=3,
                                            random_state=2,
                                            update_every=1,
                                            passes=10,
                                            alpha='auto',
                                            per_word_topics=True)
 
-pprint(lda_model.print_topics(num_words=10))
+    pprint(lda_model.print_topics(num_words=75))
+
+    # Write to file using pprint
+    with open("data_out/lda/"+doc[1]+"_topics.txt", "wt") as text_file:
+        pprint(lda_model.print_topics(num_words=50), stream=text_file)
+
+    lda_model.clear() # clear memory
